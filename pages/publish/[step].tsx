8ffe9727adf80b9e9a47f4281a7540c066ee3c0a
Anush Mutyala <69090449+anmutechsupport@users.noreply.github.com>
2022-08-21 16:01:07 -0700

    dynamic routing

--- pages/publish/[step].tsx
+++ pages/publish/[step].tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
-import Publish from "../src/components/Publish";
+import Publish from "../../src/components/Publish";
 import Page from "@shared/Page";
-import content from "../content/publish/index.json";
+import content from "../../content/publish/index.json";
 import router from "next/router";
 
 export default function PagePublish(): ReactElement {
