4c6eacd6f41cf8a6b84ca16ff2cb6afcaf57996b
Nicholas Singh <nicholas1singh@gmail.com>
2022-09-04 21:59:36 -0400

    added layout to asset page

--- pages/asset/[did]/edit.tsx
+++ pages/asset/[did]/edit.tsx
@@ -3,12 +3,16 @@ import { useRouter } from "next/router";
 import EditPage from "components/Asset/Edit";
 import AssetProvider from "@context/Asset";
 
+import Layout from "../../../components/Layout";
+
 export default function PageEditAsset(): ReactElement {
   const router = useRouter();
   const { did } = router.query;
   return (
-    <AssetProvider did={did as string}>
-      <EditPage uri={router.pathname} />
-    </AssetProvider>
+    <Layout title="Edit Asset">
+      <AssetProvider did={did as string}>
+        <EditPage uri={router.pathname} />
+      </AssetProvider>
+    </Layout>
   );
 }
--- pages/asset/[did]/index.tsx
+++ pages/asset/[did]/index.tsx
@@ -3,12 +3,16 @@ import { useRouter } from "next/router";
 import PageTemplateAssetDetails from "components/Asset";
 import AssetProvider from "@context/Asset";
 
+import Layout from "../../../components/Layout";
+
 export default function PageAssetDetails(): ReactElement {
   const router = useRouter();
   const { did } = router.query;
   return (
-    <AssetProvider did={did as string}>
-      <PageTemplateAssetDetails uri={router.asPath} />
-    </AssetProvider>
+    <Layout title="View Asset">
+      <AssetProvider did={did as string}>
+        <PageTemplateAssetDetails uri={router.asPath} />
+      </AssetProvider>
+    </Layout>
   );
 }
