ccd5c3492902d9f451eee8618344c20c5f464db4
Nicholas Singh <nicholas1singh@gmail.com>
2022-09-04 21:11:02 -0400

    added publish page

--- pages/publish/[step].tsx
+++ pages/publish/[step].tsx
@@ -3,18 +3,21 @@ import Publish from "../../src/components/Publish";
 import Page from "@shared/Page";
 import content from "../../content/publish/index.json";
 import router from "next/router";
+import Layout from "../../components/Layout";
 
 export default function PagePublish(): ReactElement {
   const { title, description } = content;
 
   return (
-    <Page
-      title={title}
-      description={description}
-      uri={router.route}
-      noPageHeader
-    >
-      <Publish content={content} />
-    </Page>
+    <Layout title="Publish">
+      <Page
+        title={title}
+        description={description}
+        uri={router.route}
+        noPageHeader
+      >
+        <Publish content={content} />
+      </Page>
+    </Layout>
   );
 }
