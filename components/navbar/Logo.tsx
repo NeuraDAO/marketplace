1c138ccd10bcbad1a804464cc00ba2a23213bb44
Anush Mutyala <69090449+anmutechsupport@users.noreply.github.com>
2022-09-11 15:08:28 -0400

    link back to main page from header

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,14 +1,17 @@
-import { Flex, Spacer, Text } from "@chakra-ui/react";
+import { Flex, Spacer, Text, Link } from "@chakra-ui/react";
 import Image from "next/image";
+// import Link from "next/link";
 
 const Logo = (props) => {
   return (
-    <Flex alignItems="center" justifyContent="space-between" {...props}>
-      <Image width={40} height={40} src="/logo.png" alt="logo" />
-      <Text p={0} fontSize="3xl" fontWeight="600">
-        NeuraDAO
-      </Text>
-    </Flex>
+    <Link href={"/?sort=nft.created&sortOrder=desc"}>
+      <Flex alignItems="center" justifyContent="space-between" {...props}>
+        <Image width={40} height={40} src="/logo.png" alt="logo" />
+        <Text p={0} fontSize="3xl" fontWeight="600">
+          NeuraDAO
+        </Text>
+      </Flex>
+    </Link>
   );
 };
 
