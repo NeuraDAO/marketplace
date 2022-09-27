e52d82c816aaf88698727f51d7543ab63cbc6887
Anush Mutyala <69090449+anmutechsupport@users.noreply.github.com>
2022-09-27 12:56:20 -0400

    added new metadata input fields in publish flow

--- src/components/Publish/_constants.tsx
+++ src/components/Publish/_constants.tsx
@@ -1,133 +1,134 @@
-import React from 'react'
-import { allowDynamicPricing, allowFixedPricing } from '../../../app.config.js'
-import { FormPublishData, PublishFeedback, StepContent } from './_types'
-import content from '../../../content/publish/form.json'
-import PricingFields from './Pricing'
-import MetadataFields from './Metadata'
-import ServicesFields from './Services'
-import Preview from './Preview'
-import Submission from './Submission'
-import { ServiceComputeOptions } from '@neuradao/ocean-lib'
-import contentFeedback from '../../../content/publish/feedback.json'
+import React from "react";
+import { allowFixedPricing } from "../../../app.config.js";
+import {
+  FormPublishData,
+  MetadataAlgorithmContainer,
+  PublishFeedback,
+  StepContent,
+} from "./_types";
+import content from "../../../content/publish/form.json";
+import PricingFields from "./Pricing";
+import MetadataFields from "./Metadata";
+import ServicesFields from "./Services";
+import Preview from "./Preview";
+import Submission from "./Submission";
+import { ServiceComputeOptions } from "@neuradao/ocean-lib";
+import contentFeedback from "../../../content/publish/feedback.json";
 
 export const wizardSteps: StepContent[] = [
   {
     step: 1,
     title: content.metadata.title,
-    component: <MetadataFields />
+    component: <MetadataFields />,
   },
   {
     step: 2,
     title: content.services.title,
-    component: <ServicesFields />
+    component: <ServicesFields />,
   },
   {
     step: 3,
     title: content.pricing.title,
-    component: <PricingFields />
+    component: <PricingFields />,
   },
   {
     step: 4,
     title: content.preview.title,
-    component: <Preview />
+    component: <Preview />,
   },
   {
     step: 5,
     title: content.submission.title,
-    component: <Submission />
-  }
-]
+    component: <Submission />,
+  },
+];
 
 const computeOptions: ServiceComputeOptions = {
   allowRawAlgorithm: false,
   allowNetworkAccess: true,
   publisherTrustedAlgorithmPublishers: [],
-  publisherTrustedAlgorithms: []
-}
+  publisherTrustedAlgorithms: [],
+};
 
 export const initialValues: FormPublishData = {
   user: {
     stepCurrent: 1,
     chainId: 1,
-    accountId: ''
+    accountId: "",
   },
   metadata: {
-    nft: { name: '', symbol: '', description: '', image_data: '' },
+    nft: { name: "", symbol: "", description: "", image_data: "" },
     transferable: true,
-    type: 'dataset',
-    name: '',
-    author: '',
-    description: '',
-    tags: '',
+    type: "dataset",
+    name: "",
+    author: "",
+    description: "",
+    tags: "",
+    modalities: "",
+    tasks: "",
+    size: "0",
+    fileCount: "0",
+    sessions: "0",
+    participants: "0",
+    participantsAge: "",
     termsAndConditions: false,
-    dockerImage: '',
-    dockerImageCustom: '',
-    dockerImageCustomTag: '',
-    dockerImageCustomEntrypoint: ''
+    dockerImage: "",
+    dockerImageCustom: "",
+    dockerImageCustomTag: "",
+    dockerImageCustomEntrypoint: "",
   },
   services: [
     {
-      files: [{ url: '' }],
-      links: [{ url: '' }],
-      dataTokenOptions: { name: '', symbol: '' },
-      timeout: '',
-      access: 'access',
+      files: [{ url: "" }],
+      links: [{ url: "" }],
+      dataTokenOptions: { name: "", symbol: "" },
+      timeout: "",
+      access: "access",
       providerUrl: {
-        url: 'https://provider.mainnet.oceanprotocol.com',
+        url: "https://provider.mainnet.oceanprotocol.com",
         valid: true,
-        custom: false
+        custom: false,
       },
-      computeOptions
-    }
+      computeOptions,
+    },
   ],
   pricing: {
+    baseToken: { address: "", name: "", symbol: "OCEAN", decimals: 18 },
     price: 0,
-    type: allowFixedPricing === 'true' ? 'fixed' : 'free',
-    amountDataToken: 1000,
-    amountOcean: 100,
-    weightOnOcean: '5', // 50% on OCEAN
-    weightOnDataToken: '5', // 50% on datatoken
-    swapFee: 0.1, // in %
-    freeAgreement: false
-  }
-}
-
-export interface MetadataAlgorithmContainer {
-  entrypoint: string
-  image: string
-  tag: string
-  checksum: string
-}
+    type: allowFixedPricing === "true" ? "fixed" : "free",
+    freeAgreement: false,
+  },
+};
 
 export const algorithmContainerPresets: MetadataAlgorithmContainer[] = [
   {
-    image: 'node',
-    tag: 'latest',
-    entrypoint: 'node $ALGO',
+    image: "node",
+    tag: "18.6.0", // TODO: Put this back to latest once merging the PR that fetches the container digest from docker hub via dockerhub-proxy
+    entrypoint: "node $ALGO",
     checksum:
-      'sha256:eee4c74962a855bc1694676cd5069460e545a8580e0e574b2c59eae4f40bd5bd' // TODO: how to get? Most likely needs to be fetched from DockerHub.
+      "sha256:c60726646352202d95de70d9e8393c15f382f8c6074afc5748b7e570ccd5995f",
   },
   {
-    image: 'python',
-    tag: 'latest',
-    entrypoint: 'python $ALGO',
+    image: "python",
+    tag: "3.10.5", // TODO: Put this back to latest once merging the PR that fetches the container digest from docker hub via dockerhub-proxy
+    entrypoint: "python $ALGO",
     checksum:
-      'sha256:607635763e54907fd75397fedfeb83890e62a0f9b54a1d99d27d748c5d269be4'
+      "sha256:607635763e54907fd75397fedfeb83890e62a0f9b54a1d99d27d748c5d269be4",
   },
   {
-    image: 'anmu06/c2d_neuradao',
-    tag: 'latest',
-    entrypoint: 'python3.8 $ALGO',
+    image: "anmu06/c2d_neuradao",
+    tag: "latest",
+    entrypoint: "python3.8 $ALGO",
     checksum:
-      'sha256:a981ed6282271fc5492c382cd11d5045641880f738c05a855ed6de8d0eecea8f'
+      "sha256:a981ed6282271fc5492c382cd11d5045641880f738c05a855ed6de8d0eecea8f",
   },
   {
-    entrypoint: 'python3.7 $ALGO',
-    image: 'oceanprotocol/algo_dockers',
-    tag: 'python-panda',
+    entrypoint: "python3.7 $ALGO",
+    image: "oceanprotocol/algo_dockers",
+    tag: "python-panda",
     checksum:
-      'sha256:7fc268f502935d11ff50c54e3776dda76477648d5d83c2e3c4fdab744390ecf2'
-  }
-]
+      "sha256:7fc268f502935d11ff50c54e3776dda76477648d5d83c2e3c4fdab744390ecf2",
+  },
+];
 
-export const initialPublishFeedback: PublishFeedback = contentFeedback
+export const initialPublishFeedback: PublishFeedback = contentFeedback;
--- src/components/Publish/_types.ts
+++ src/components/Publish/_types.ts
@@ -1,65 +1,78 @@
-import { ServiceComputeOptions } from '@neuradao/ocean-lib'
-import { NftMetadata } from '@utils/nft'
-import { ReactElement } from 'react'
-import { PriceOptions } from 'src/@types/Price'
+import { ServiceComputeOptions } from "@neuradao/ocean-lib";
+import { NftMetadata } from "@utils/nft";
+import { ReactElement } from "react";
 
 interface FileInfo {
-  url: string
-  valid?: boolean
-  contentLength?: string
-  contentType?: string
+  url: string;
+  valid?: boolean;
+  contentLength?: string;
+  contentType?: string;
 }
 
 export interface FormPublishService {
-  files: FileInfo[]
-  links?: FileInfo[]
-  timeout: string
-  dataTokenOptions: { name: string; symbol: string }
-  access: 'Download' | 'Compute' | string
-  providerUrl: { url: string; valid: boolean; custom: boolean }
-  algorithmPrivacy?: boolean
-  computeOptions?: ServiceComputeOptions
+  files: FileInfo[];
+  links?: FileInfo[];
+  timeout: string;
+  dataTokenOptions: { name: string; symbol: string };
+  access: "Download" | "Compute" | string;
+  providerUrl: { url: string; valid: boolean; custom: boolean };
+  algorithmPrivacy?: boolean;
+  computeOptions?: ServiceComputeOptions;
 }
 
 export interface FormPublishData {
   user: {
-    stepCurrent: number
-    accountId: string
-    chainId: number
-  }
+    stepCurrent: number;
+    accountId: string;
+    chainId: number;
+  };
   metadata: {
-    nft: NftMetadata
-    transferable: boolean
-    type: 'dataset' | 'algorithm'
-    name: string
-    description: string
-    author: string
-    termsAndConditions: boolean
-    tags?: string
-    dockerImage?: string
-    dockerImageCustom?: string
-    dockerImageCustomTag?: string
-    dockerImageCustomEntrypoint?: string
-    dockerImageCustomChecksum?: string
-  }
-  services: FormPublishService[]
-  pricing: PriceOptions
-  feedback?: PublishFeedback
+    nft: NftMetadata;
+    transferable: boolean;
+    type: "dataset" | "algorithm";
+    name: string;
+    description: string;
+    author: string;
+    termsAndConditions: boolean;
+    tags?: string;
+    modalities?: string;
+    tasks?: string;
+    size?: string;
+    fileCount?: string;
+    sessions?: string;
+    participants?: string;
+    participantsAge?: string;
+    dockerImage?: string;
+    dockerImageCustom?: string;
+    dockerImageCustomTag?: string;
+    dockerImageCustomEntrypoint?: string;
+    dockerImageCustomChecksum?: string;
+  };
+  services: FormPublishService[];
+  pricing: PricePublishOptions;
+  feedback?: PublishFeedback;
 }
 
 export interface StepContent {
-  step: number
-  title: string
-  component: ReactElement
+  step: number;
+  title: string;
+  component: ReactElement;
 }
 
 export interface PublishFeedback {
   [key: string]: {
-    name: string
-    description: string
-    status: 'success' | 'error' | 'pending' | 'active' | string
-    txCount: number
-    errorMessage?: string
-    txHash?: string
-  }
+    name: string;
+    description: string;
+    status: "success" | "error" | "pending" | "active" | string;
+    txCount: number;
+    errorMessage?: string;
+    txHash?: string;
+  };
+}
+
+export interface MetadataAlgorithmContainer {
+  entrypoint: string;
+  image: string;
+  tag: string;
+  checksum: string;
 }
