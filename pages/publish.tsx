81e094ffa86a196e77975a17b5d3077e97422294
Nicholas Singh <nicholas1singh@gmail.com>
2022-10-23 22:52:54 -0400

    integrated basic upload

--- pages/publish.tsx
+++ pages/publish.tsx
@@ -1,5 +1,5 @@
 import { getSupabase } from "../utils/supabase";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import {
   FormControl,
   FormLabel,
@@ -46,7 +46,9 @@ function TextInput({
 const Publish = () => {
   const [values, setValues] = useState({});
 
-  function setValue(name, value) {
+  const { user } = useUser();
+
+  function setValue(value, name) {
     setValues({ ...values, [name]: value });
   }
 
@@ -54,122 +56,129 @@ const Publish = () => {
     return values[name] || "";
   }
 
+  const uploadMetadata = async () => {
+    const supabase = getSupabase();
+    if (!values || !user) return;
+    // TODO: note this should be removed as soon as possible since it can expose a security vunerability
+    const resp = await supabase
+      .from("metadata")
+      .insert({ owner: user.sub, ...values });
+    console.log({ resp });
+
+    setValues({});
+  };
+
   return (
     <Layout title="Publish">
       {/* centers the grid on the page */}
       <Center>
         <Grid>
           <TextInput
-            name="owner"
-            value={getValue("owner")}
-            setValue={(val) => setValue(val, name)}
-          />
-          <TextInput
             name="language"
             value={getValue("language")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "language")}
           />
           <TextInput
             name="version"
             value={getValue("version")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "version")}
           />
           <TextInput
             name="description"
             value={getValue("description")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "description")}
           />
           <TextInput
             name="name"
             value={getValue("name")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "name")}
           />
           <TextInput
             name="type"
             value={getValue("type")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "type")}
           />
           <TextInput
             name="author"
             value={getValue("author")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "author")}
           />
           <TextInput
             name="license"
             value={getValue("license")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "license")}
           />
           <TextInput
             name="links"
             value={getValue("links")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "links")}
           />
           <TextInput
             name="tags"
             value={getValue("tags")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "tags")}
           />
           <TextInput
             name="modalities"
             value={getValue("modalities")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "modalities")}
           />
           <TextInput
             name="tasks"
             value={getValue("tasks")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "tasks")}
           />
           <TextInput
             name="size"
             value={getValue("size")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "size")}
           />
           <TextInput
             name="files"
             value={getValue("files")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "files")}
           />
           <TextInput
             name="sessions"
             value={getValue("sessions")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "sessions")}
           />
           <TextInput
             name="participants"
             value={getValue("participants")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "participants")}
           />
           <TextInput
             name="participantsAge"
             value={getValue("participantsAge")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "participantsAge")}
           />
           <TextInput
             name="categories"
             value={getValue("categories")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "categories")}
           />
           <TextInput
             name="copyrightHolder"
             value={getValue("copyrightHolder")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "copyrightHolder")}
           />
           <TextInput
             name="contentLanguage"
             value={getValue("contentLanguage")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "contentLanguage")}
           />
           <TextInput
             name="algorithm"
             value={getValue("algorithm")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "algorithm")}
           />
           <TextInput
             name="additionalInformation"
             value={getValue("additionalInformation")}
-            setValue={(val) => setValue(val, name)}
+            setValue={(val) => setValue(val, "additionalInformation")}
           />
-          <Button onClick={() => console.log(values)}>Submit</Button>
+          <Button onClick={uploadMetadata}>Submit</Button>
         </Grid>
       </Center>
     </Layout>
